eb4942ca16abc90dc80e32e6d16223d496184f8c
yunxchung <yunxchung@gmail.com>
2021-03-16 23:36:46 +0900

    feat: Modify valid_palindrome solution

--- problems/valid_palindrome.py
+++ problems/valid_palindrome.py
@@ -4,17 +4,22 @@ import re
 
 def is_palindrome(self, s: str) -> bool:
 
+    s = s.lower()
+
     # 특수문자 및 공백 제거
-    s = re.sub(r"[^a-zA-Z0-9]", "", s).lower()
+    s = re.sub(r'[^a-z0-9]','', s)
 
-    # 뒤집힌 char 집합 생성
-    chars = list(s)
-    chars.reverse()
+    # # 뒤집힌 char 집합 생성
+    # chars = list(s)
+    # chars.reverse()
+    #
+    # # 뒤집힌 문자열 생성
+    # s_reverse = "".join([char for char in chars])
+    #
+    # return s == s_reverse
 
-    # 뒤집힌 문자열 생성
-    s_reverse = "".join([char for char in chars])
-
-    return s == s_reverse
+    # 슬라이싱
+    return s == s[::-1]
 
 
 if __name__ == "__main__":
