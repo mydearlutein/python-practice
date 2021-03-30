4d9c3a2972b35d64ede40cbaea7ee1f4f1ddd5a6
yunxchung <yunxchung@gmail.com>
2021-03-30 22:59:29 +0900

    feat: Add merge_two_sorted_lists problem

---
+++ problems/merge_two_sorted_lists.py
@@ -0,0 +1,7 @@
+def merge_two_sorted_lists(self, l1: ListNode, l2: ListNode) -> ListNode:
+    if (not l1) or (l2 and l1.val > l2.val):
+        l1, l2 = l2, l1
+    if l1:
+        l1.next = self.merge_two_sorted_lists(l1.next, l2)
+    return l1
+    
